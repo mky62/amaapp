7b74879fb43b38c618515ec5dfe6c906fbd033b3
Abhishek <abhiskul00@gmail.com>
2026-02-28 14:53:55 +0530

    feat: add sign-in functionality with email and password
    - Updated package.json to include new dependencies for email rendering and UI components.
    - Created SignInForm component for user authentication.
    - Implemented loading state and error handling in sign-in process.
    - Added Spinner component for loading indication.
    - Enhanced global styles with new CSS variables and Tailwind integration.
    - Introduced utility functions for class name management.
    - Added components.json for Shadcn configuration.

---
+++ src/app/(auth)/signIn/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SignInForm from '@/components/SignInForm'
+
+function page() {
+  return (
+    <div
+    className='flex min-h-screen items-center justify-center'>
+        <SignInForm />
+    </div>
+
+)
+}
+
+export default page
